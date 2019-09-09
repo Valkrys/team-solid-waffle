1a1547ad9b4d23ee2a54c39d8d524a6deab1911c
Reece Bennett <reeceb@kainos.com>
2019-09-09 13:28:46 +0100

    Create repository structure
    Added Angular app, Express app and .gitignore

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// Load configuration from .env
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 8002;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
