8148264ced9f574f0965acad081988929ab45f20
Rewaz <klv96@outlook.com>
2019-09-10 14:53:32 +0100

    made the timeline

--- app/src/app/details-page/timeline/timeline.component.ts
+++ app/src/app/details-page/timeline/timeline.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimelineComponent implements OnInit {
 
+  private data = [
+    { passed: true, title: 'Gathering Information' },
+    { passed: true, title: 'Planning' },
+    { passed: false, title: 'Design' },
+    { passed: false, title: 'Content Writing and Assembly' },
+    { passed: false, title: 'Coding' },
+    { passed: false, title: 'Testing, Review & Launch' },
+    { passed: false, title: 'Maintenance' }
+  ];
+  
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
+
+
