997c4ccfdcb024bfe03cf56a84bb3e8ad8310d1b
Rewaz <klv96@outlook.com>
2019-09-18 10:44:40 +0100

    added dataService

--- app/src/app/data.service.ts
+++ app/src/app/data.service.ts
@@ -73,10 +73,6 @@ export class DataService {
 
   public loggedIn()
   {
-    console.log("***********************");
-    
-    console.log(document.cookie);
-    
     return !!localStorage.getItem('token');
   }
 
